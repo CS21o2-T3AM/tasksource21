ac910c06dde5aadb699190960bb65076663af0cf
JonnDiv <angjonnas@gmail.com>
2017-10-14 01:04:27 +0800

    Updated with User Account viewing, fixed borders

--- htdocs/admin.php
+++ htdocs/admin.php
@@ -22,11 +22,10 @@ if($email==""){
 </head>
 <body>
 <h1>Admin Control Panel</h1>
-<h2>Welcome back, <?php$name?>!</h2>
+<h3 align="right">Welcome back, <?php echo $name; ?>!</h3>
 <form name="home" action="home.php" method="POST">
-    <li><input type="submit" name="logout" value="Logout" style="position: absolute; right: 0;"/></li>
+    <input type="submit" name="logout" value="Logout" style="position: absolute; right: 0;"/>
 </form>
-<h2>All Bids</h2>
 
 <?php
 
@@ -38,6 +37,8 @@ if (isset($_POST['logout'])){
 ?>
 <style>
     table {
+        border: 2px solid lightgray;
+        border-radius: 5px;
         border-collapse: collapse;
         width: 100%;
     }
@@ -50,9 +51,9 @@ if (isset($_POST['logout'])){
 
     tr:hover{background-color:#f5f5f5}
 </style>
-<div STYLE=" height: 300px; width: auto; font-size: 18px; overflow: auto;">
-    <?php
 
+
+    <?php
     //Connect to Database
     try {
         $dbuser = 'postgres';
@@ -66,7 +67,72 @@ if (isset($_POST['logout'])){
         die();
     }
 
-    //SHOW BIDS
+    //All USERS
+    echo "<h2>User Accounts</h2>";
+    echo "<form name='searchUsers' action='' method='POST'>";
+    echo   "<li>Search Users(Email/Name): ";
+    echo      "<input type='text' name='userName' value=''/>";
+    echo    "<input type='submit' name='searchUsers' value='Search'>";
+    echo"</form>";
+
+    $_POST['searchUsers'] = true;
+    if (isset($_POST['searchUsers'])) {
+        echo $_POST['userName'];
+
+        $userInput = $_POST['userName'];
+        //Dynamically display bids
+        echo "<div STYLE=\" height: 300px; width: auto; font-size: 16px; overflow: auto; border:solid 2px darkgray; border-radius:5px;\">";
+        //Display all by default
+        $sql = 'select * from users';
+
+        if(strpos($userInput, '@')){
+            //Search by email
+            echo "Searching by Email: ".$userInput;
+            //Query using ILIKE
+            $sql = 'select * from users where email ILIKE '."'%".$userInput."%'";
+        }
+        else if(!empty($userInput)){
+            //Search by bidName
+            echo "Searching by User's Name: ".$userInput;
+            $sql = 'select * from users where name ILIKE '."'%".$userInput."%'";
+        }
+        else{
+            //If all else fails, display default
+            echo "Showing All Bids";
+            $sql = 'select * from users';
+        }
+
+        echo "<table>";
+        echo "<tr>";
+        echo "<th align='center' width='200'>Email</th>";
+        echo "<th align='center' width='200'>Name</th>";
+        echo "<th align='center' width='100'>Date Of Birth</th>";
+        echo "<th align='center' width='10'>Administrative Rights</th>";
+        echo "<th align='center' width='50'>Phone</th>";
+
+        foreach ($connec->query($sql) as $row)
+        {
+            echo "<tr>";
+            echo "<td align='center' width='200'><a href=\"CreateBid.php?taskid={$row['taskid']}&owneremail={$row['owneremail']}&useremail={$email}\">".$row['email']."</a></td>";
+            echo "<td align='center' width='200'>" . $row['name'] . "</td>";
+            echo "<td align='center' width='200'>" . $row['dateofbirth'] . "</td>";
+            echo "<td align='center' width='200'>" . $row['admin'] . "</td>";
+            echo "<td align='center' width='200'>" . $row['phone'] . "</td>";
+            echo "</tr>";}
+
+        echo "</table>";
+        echo "</div>";
+    }
+
+    echo "</table>";
+    echo "</div>";
+
+    echo "<br/>";
+    echo "<br/>";
+
+
+    //ALL BIDS
+    echo "<h2>User Bids</h2>";
     echo "<form name='searchBids' action='' method='POST'>";
     echo   "<li>Search Bids (Email/Bid Name): ";
     echo      "<input type='text' name='bidName' value=''/>";
@@ -79,7 +145,7 @@ if (isset($_POST['logout'])){
 
         $userInput = $_POST['bidName'];
         //Dynamically display bids
-        echo "<div STYLE=\" height: 300px; width: auto; font-size: 18px; overflow: auto;\">";
+        echo "<div STYLE=\" height: 300px; width: auto; font-size: 16px; overflow: auto;border:2px solid darkgray; border-radius:5px;\">";
         //Display all by default
         $sql = 'select * from bid_task, create_task where bt.taskid = ct.taskid';
 
@@ -141,7 +207,7 @@ if (isset($_POST['logout'])){
     if (isset($_POST['searchTasks'])) {
         echo $_POST['taskName'];
 
-        echo "<div STYLE=\" height: 300px; width: auto; font-size: 18px; overflow: auto;\">";
+        echo "<div STYLE=\" height: 300px; width: auto; font-size: 16px; overflow: auto;border:2px solid darkgray; border-radius:5px;\">";
         $sql = 'SELECT * FROM create_task where owneremail != '."'$email'"."AND taskname ILIKE'%".$_POST['taskName']."%'";
         echo "<table>";
         echo "<tr>";
