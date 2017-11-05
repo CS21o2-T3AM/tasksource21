d47596f7cb0913e331e7fb978a03a982834475b9
JonnDiv <angjonnas@gmail.com>
2017-11-06 00:33:27 +0800

    Deleted welcome msg

--- htdocs/admintasksearch.php
+++ htdocs/admintasksearch.php
@@ -72,8 +72,6 @@ admin_login_validate_or_redirect();
 </div>
 
 <div class="container" >
-    <!--welcome message-->
-    <h3 align="right">Welcome back, <?php echo $email; ?>!</h3>
 
     <!-- Menu -->
     <div  align='right' class='container' id="wrapper" style="">
